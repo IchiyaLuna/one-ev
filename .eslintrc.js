716eb64924e418de92a129069acf10c4cdb2060b
Ichiya Luna <akkyu0724@gmail.com>
2022-02-13 00:07:06 +0900

    reset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
-    '@vue/airbnb',
+    'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-param-reassign': 0, // 파라미터는 지역변수로 받아서 쓰라는 rule
-    'no-unused-vars': 0, // 파라미터는 지역변수로 받아서 쓰라는 rule
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'import/prefer-default-export': 'off',
-    'import/extensions': ['off'],
-  },
-};
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
--- src/plugins/bootstrap-vue.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-import BootstrapVue from 'bootstrap-vue-3';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-
-Vue.use(BootstrapVue);
