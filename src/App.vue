ac7b41520d1462e28dd0b0d85c78bf1ff37fbcf8
Ichiya Luna <akkyu0724@gmail.com>
2022-05-03 16:55:03 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,57 +5,13 @@
 <script>
   export default {
     data() {
-      return {
-        routeData: {
-          rootMenu: ["home", "student", "consult", "class", "teacher", "academy"],
-          home: {
-            name: "Home",
-            icon: "fa-regular fa-house ",
-            path: "/home",
-          },
-          student: {
-            name: "학생 관리",
-            icon: "fa-regular fa-address-card",
-            path: "/student",
-            children: [
-              {
-                path: "timeline",
-              },
-              {
-                path: "/consult-history",
-              },
-            ],
-          },
-          consult: {
-            name: "상담 관리",
-            icon: "fa-regular fa-clipboard-check",
-            path: "/consult",
-          },
-          class: {
-            name: "수업 관리",
-            icon: "fa-regular fa-screen-users",
-            path: "/class",
-          },
-          teacher: {
-            name: "강사 관리",
-            icon: "fa-regular fa-chalkboard-user",
-            path: "/teacher",
-          },
-          academy: {
-            name: "학원 관리",
-            icon: "fa-regular fa-school",
-            path: "/academy",
-          },
-        },
-      };
+      return {};
     },
     provide() {
       return {
         appData: {
           version: "v0.0.2",
         },
-        API_KEY: "f9905e3c4a683ab33e07b8b75f5ec96a",
-        routeData: this.routeData,
       };
     },
   };
